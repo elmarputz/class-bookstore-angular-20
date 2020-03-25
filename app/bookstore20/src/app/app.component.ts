390be7525ef6e2ea6477fd34c14282af52628e84
Elmar Putz <ep@studiomitte.com>
2020-03-25 22:51:31 +0100

    ServiceProvider & Routing

--- app/bookstore20/src/app/app.component.ts
+++ app/bookstore20/src/app/app.component.ts
@@ -1,28 +1,8 @@
 import { Component } from '@angular/core';
-import { Book } from './shared/book';
 
 @Component({
   selector: 'bs-root',
-  template: '<bs-book-list *ngIf="listOn" (showDetailsEvent)="showDetails($event)"></bs-book-list><bs-book-details (showListEvent)="showList()" *ngIf="detailsOn" [book]="book"></bs-book-details>',
-  styles: []
+  templateUrl: './app.component.html'
 })
 
-export class AppComponent {
-  title = 'bookstore20';
-
-  listOn = true;
-  detailsOn = false;
-  book : Book;
-
-  showList() {
-    this.listOn = true;
-    this.detailsOn = false;
-  }
-
-  showDetails(book: Book) {
-    this.book = book;
-    this.listOn = false;
-    this.detailsOn = true;
-  }
-
-}
+export class AppComponent { }
